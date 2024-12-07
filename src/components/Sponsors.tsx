946c88ecf7b24108e64dc0f0ccbd27db60646ff4
Sanket Y <124805576+sanketyelave@users.noreply.github.com>
2024-12-07 18:54:50 +0530

    Update Sponsors.tsx

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { sponsorData } from '../Data/SponsorData'
 function Sponsors() {
   return (
-    <div className='pb-[4rem]' id='Sponsors'>
+    <div className='pb-[4rem]' id='Sponsors' className="bg-black">
 
 
   <div className="relative font-inter antialiased">
